a9558aa3469334f7e1e15f73ad8b354a46b1b92d
LaboratoryRaccoon <71449590+LaboratoryRaccoon@users.noreply.github.com>
2021-01-31 10:44:30 +0300

    shield and spear

--- Items/Weapons/TowerKnightsSpear.cs
+++ Items/Weapons/TowerKnightsSpear.cs
@@ -10,26 +10,7 @@ namespace TerraSouls.Items.Weapons
     {
         public override void SetDefaults()
         {
-            item.name = "Копье Рыцаря Башни";
-            item.damage = 99;
-            item.toolTip = "Копье одного из могучих демонов - Рыцаря Башни. Он охранял замок Террарии от иноземцев и врагов. \nКопье имеет очень сильный урон по пешим войскам, но, тем не менее, очень тяжелое.";
-            item.melee = true;
-            item.width = 36;
-            item.height = 36;
-            item.scale = 1.1f;
-            item.maxStack = 1;
-            item.useTime = 45;
-            item.useAnimation = 45;
-            item.knockBack = 6f;
-            item.useSound = 1;
-            item.noMelee = true;
-            item.noUseGraphic = true;
-            item.useTurn = true;
-            item.useStyle = 5;
-            item.rare = 3;
-            item.value = Item.sellPrice(0, 1, 0, 0);
-            item.shoot = mod.ProjectileType("TowerKnight`Spear");
-            item.shootSpeed = 7f;
+            
         }
     }
 }
